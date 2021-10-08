1254addfcb40e53247d852bf8d3b894df013cd00
fakhriahmad3112 <63622491+fakhriahmad3112@users.noreply.github.com>
2021-10-08 20:34:43 +0700

    Menambahkan beberapa file dan mengubahnya

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,93 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Fakhri Ahmad",
         "email" => "193040123.fakhri@unpas.ac.id",
         "image" => "fakhri.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Fakhri Ahmad",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Laborum dolor illum harum accusantium dolorem modi ducimus ab! Perferendis harum qui neque temporibus. 
+        Voluptatum in alias quas ea nihil nostrum est excepturi, perferendis impedit quam aperiam eaque dolorum 
+        necessitatibus odit a. Natus perferendis voluptates nemo nesciunt nihil necessitatibus! Nemo ratione adipisci 
+        recusandae sapiente, quod tempora veniam nam minima fugiat necessitatibus, voluptas commodi debitis aliquid in 
+        saepe possimus voluptatum asperiores obcaecati. Error in iure adipisci, magnam quidem sequi quod laborum illum esse."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "zaskha sasmita",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Ipsum, veritatis labore harum recusandae iusto, enim soluta qui est eligendi perspiciatis maxime quis 
+        dolorum alias vel deleniti ab quaerat tempore porro. Saepe dolores consequatur, ipsum eum, deleniti 
+        accusantium quos aut fugiat placeat minima cupiditate laboriosam! Deserunt, possimus, magni corrupti 
+        dignissimos commodi accusamus adipisci dicta asperiores voluptate natus explicabo? Incidunt sapiente quia 
+        laboriosam iste, id porro suscipit. Molestias cupiditate maiores ipsum, esse doloribus enim ratione dignissimos 
+        ut nisi nulla harum quo eius laborum nobis autem, non ipsam magni, voluptatem nostrum. Sit nihil cum 
+        exercitationem nesciunt provident laboriosam. Libero excepturi quam deleniti tempore."
+        ]
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halaman single post
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Fakhri Ahmad",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Laborum dolor illum harum accusantium dolorem modi ducimus ab! Perferendis harum qui neque temporibus. 
+        Voluptatum in alias quas ea nihil nostrum est excepturi, perferendis impedit quam aperiam eaque dolorum 
+        necessitatibus odit a. Natus perferendis voluptates nemo nesciunt nihil necessitatibus! Nemo ratione adipisci 
+        recusandae sapiente, quod tempora veniam nam minima fugiat necessitatibus, voluptas commodi debitis aliquid in 
+        saepe possimus voluptatum asperiores obcaecati. Error in iure adipisci, magnam quidem sequi quod laborum illum esse."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "zaskha sasmita",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Ipsum, veritatis labore harum recusandae iusto, enim soluta qui est eligendi perspiciatis maxime quis 
+        dolorum alias vel deleniti ab quaerat tempore porro. Saepe dolores consequatur, ipsum eum, deleniti 
+        accusantium quos aut fugiat placeat minima cupiditate laboriosam! Deserunt, possimus, magni corrupti 
+        dignissimos commodi accusamus adipisci dicta asperiores voluptate natus explicabo? Incidunt sapiente quia 
+        laboriosam iste, id porro suscipit. Molestias cupiditate maiores ipsum, esse doloribus enim ratione dignissimos 
+        ut nisi nulla harum quo eius laborum nobis autem, non ipsam magni, voluptatem nostrum. Sit nihil cum 
+        exercitationem nesciunt provident laboriosam. Libero excepturi quam deleniti tempore."
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
